2f0e68e560518aeea8a0f4210cef90d6266bad27
Carlos Eugenio Torres <cetorresusa@gmail.com>
2022-01-24 23:47:37 -0700

    Layout improve.

--- src/Home.tsx
+++ src/Home.tsx
@@ -223,7 +223,7 @@ export default function Home() {
                 </div>
                 <div className="modal-footer">
                   <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                  <button type="submit" className="btn btn-primary">Save</button>
+                  <button type="submit" className="btn btn-success">Save</button>
                 </div>
               </form>
             </div>
