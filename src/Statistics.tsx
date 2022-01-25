d332d386f10f2bd3fe682e2fe43da0bf64ca0c6a
Carlos Eugenio Torres <cetorresusa@gmail.com>
2022-01-24 23:41:47 -0700

    Improved statistics.

--- src/Statistics.tsx
+++ src/Statistics.tsx
@@ -7,8 +7,9 @@ export default function Statistics() {
   const [gamesPlayed, setGamesPlayed] = useState('');
   const [winsPercentage, setWinsPercentage] = useState('0%');
   const [numberWins, setNumberWins] = useState('');
-  const [numberLosts, setNumberLosts] = useState('');
+  const [numberLosses, setNumberLosses] = useState('');
   const [firstDate, setFirstDate] = useState('');
+  const [lastDate, setLastDate] = useState('');
   const [chartData, setChartData] = useState<any>([]);
 
   function loadStatistics(savedResults: Array<Result>) {
@@ -24,10 +25,13 @@ export default function Statistics() {
     setWinsPercentage(((wins / total) * 100).toFixed(0));
     
     setNumberWins(wins.toString());
-    setNumberLosts(losts.toString());
+    setNumberLosses(losts.toString());
 
     const firstGameDate = savedResults.sort((r1, r2) => r1.number - r2.number)[0].date.split('T')[0];
     setFirstDate(firstGameDate);
+
+    const lastGameDate = savedResults.sort((r1, r2) => r2.number - r1.number)[0].date.split('T')[0];
+    setLastDate(lastGameDate);
 
     const total_1_tries = savedResults.filter((r) => r.tries.includes('1/')).length;
     const total_2_tries = savedResults.filter((r) => r.tries.includes('2/')).length;
@@ -70,7 +74,7 @@ export default function Statistics() {
                 <h4>{ gamesPlayed }</h4>
               </div>
               <div className="card-footer text-muted">
-                Since { firstDate }
+                Last: { lastDate } - First: { firstDate }
               </div>
             </div>
           </div>
@@ -83,7 +87,7 @@ export default function Statistics() {
                 <h4>{ winsPercentage }%</h4>
               </div>
               <div className="card-footer text-muted">
-                Wins: {numberWins} - Lost: {numberLosts}
+                Wins: {numberWins} - Losses: {numberLosses}
               </div>
             </div>
           </div>
